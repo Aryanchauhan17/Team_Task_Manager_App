a4e2c32d9bea6fdd3bf80b32ed255cd03695744b
Aryanchauhan01 <aryan17chauhan@gmail.com>
2026-04-30 23:07:01 -0700

    fix: hardcode api base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://teamtaskmanagerapp-production.up.railway.app',
+  baseURL: 'https://teamtaskmanagerapp-production.up.railway.app/api',
 })
 
 API.interceptors.request.use((config) => {
